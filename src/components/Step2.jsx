7151ccd24eb609c640e495d16d1befe4fdd195c8
Silver7Surfer <quick47silver@gmail.com>
2025-03-31 21:21:19 +0545

    -minor changes

--- src/components/Step2.jsx
+++ src/components/Step2.jsx
@@ -18,15 +18,18 @@ const Step2 = ({ formData, errors, handleInputChange, goToStep, currentStep }) =
       <p className="mb-2">Enter your username from the server provider.</p>
       <p className="mb-4">Enter the amount you would like to load.</p>
       
-      <input 
-        type="text" 
-        id="username" 
-        name="username" 
+      <div>
+      <input  
+        type="text"  
+        id="username"  
+        name="username"  
         placeholder="Username"
         value={formData.username}
-        onChange={handleInputChange} 
-        className="w-full my-4 p-2 rounded bg-white text-black"
+        onChange={handleInputChange}  
+        className="w-full mt-4 mb-1 p-2 rounded bg-white text-black"
       />
+      <span className="text-xs text-gray-500">Multiple usernames are accepted seperated by comma</span>
+    </div>
       
       {errors.username && 
         <span className="text-red-500 text-xs text-left block -mt-2 mb-3">
